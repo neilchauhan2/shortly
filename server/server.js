af6a8efe0956e4b956adb363a419e9417e2307da
Neil Chauhan <neilchauhan2@gmail.com>
2019-07-18 18:18:50 +0530

    db setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const port = process.env.PORT || 8000;
+const connect = require("./config/db");
+
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const app = express();
+connect();
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
