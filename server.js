fc6b55008090f0ea4e09f8a868a680541c378708
Neil Chauhan <neilchauhan2@gmail.com>
2019-08-06 00:08:14 +0530

    update

--- server.js
+++ server.js
@@ -17,11 +17,12 @@ app.use("/api/auth", require("./routes/user"));
 
 connect();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
