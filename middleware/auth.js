b0e29dc5999edd543cfa438f611ec66f39eb93af
Neil Chauhan <neilchauhan2@gmail.com>
2020-07-20 02:00:54 +0530

    update
    Signed-off-by: Neil Chauhan <neilchauhan2@gmail.com>

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,21 @@
 const config = require("config");
 const jwt = require("jsonwebtoken");
-
+const jwtSecret = config.get("jwtSecret") || process.env.jwtSecret;
 function auth(req, res, next) {
-  const token = req.header("x-auth-token");
+    const token = req.header("x-auth-token");
 
-  // Check for token
-  if (!token) return res.status(401).send("No token, authorizaton denied");
+    // Check for token
+    if (!token) return res.status(401).send("No token, authorizaton denied");
 
-  try {
-    // Verify token
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
-    // Add user from payload
-    req.user = decoded;
-    next();
-  } catch (e) {
-    res.status(400).send("Token is not valid");
-  }
+    try {
+        // Verify token
+        const decoded = jwt.verify(token, jwtSecret);
+        // Add user from payload
+        req.user = decoded;
+        next();
+    } catch (e) {
+        res.status(400).send("Token is not valid");
+    }
 }
 
 module.exports = auth;
--- models/db.js
+++ models/db.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const uri = process.env.mongodbURI;
+const uri = config.get("mongodbURI") || process.env.mongodbURI;
 
 const connect = async () => {
-  try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true
-    });
+    try {
+        await mongoose.connect(uri, {
+            useNewUrlParser: true
+        });
 
-    console.log("mongodb connected");
-  } catch (error) {
-    throw error;
-  }
+        console.log("mongodb connected");
+    } catch (error) {
+        throw error;
+    }
 };
 
 module.exports = connect;
